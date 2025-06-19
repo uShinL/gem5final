8daeef2fc8c0b843943336e2d7870a389b852fda
uShinL <158572811+uShinL@users.noreply.github.com>
2025-06-19 22:59:59 +0800

    Update base.cc

--- gem5-525ce650e1a5bbe71c39d4b15598d6c003cc9f9e/src/mem/cache/base.cc
+++ gem5-525ce650e1a5bbe71c39d4b15598d6c003cc9f9e/src/mem/cache/base.cc
@@ -1069,10 +1069,6 @@ BaseCache::access(PacketPtr pkt, CacheBlk *&blk, Cycles &lat,
         incHitCount(pkt);
         satisfyRequest(pkt, blk);
         maintainClusivity(pkt->fromCache(), blk);
-        if (blk->isWritable()){
-	        PacketPtr writeclean_pkt = writecleanBlk(blk, pkt->req->getDest(), pkt->id);
-        	writebacks.push_back(writeclean_pkt);
-    	}
         return true;
     }
 
